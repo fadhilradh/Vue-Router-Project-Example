ced37d141eda9c9ba3e610b07190fb28234847e6
fadhilradh <fadhilradh@users.noreply.github.com>
2021-03-29 06:05:45 +0700

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,27 @@
+import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
+
+import App from './App.vue';
+import TeamsList from './components/teams/TeamsList.vue';
+import TeamMembers from './components/teams/TeamMembers.vue';
+import UsersList from './components/users/UsersList.vue';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/', redirect: '/users' },
+    {
+      path: '/teams',
+      component: TeamsList,
+      children: [{ path: ':teamId', component: TeamMembers, props: true }]
+    },
+    { path: '/users', component: UsersList },
+    { path: '/:notFound(.*)', redirect: '/teams' }
+  ]
+});
+
+const app = createApp(App);
+
+app.use(router);
+
+app.mount('#app');
